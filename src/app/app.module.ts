f8c99f03e8fbb5c545218586489d36c085bb9995
Sathyapriyan Chinnathangam Nadar <nsathya1990@gmail.com>
2021-05-22 22:17:48 +0530

    styling tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,9 +20,10 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent, DetailsComponent, GameTabsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
